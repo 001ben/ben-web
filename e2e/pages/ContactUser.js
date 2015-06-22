8e4f2e3b3f635053275b327d96df5684706e7934
ben <bufflanky@gmail.com>
2015-06-22 20:32:58 +1000

    Fixed indenting, live preview working great
    This is the base commit where the default template is definitely working

--- e2e/pages/ContactUser.js
+++ e2e/pages/ContactUser.js
@@ -1,22 +1,21 @@
-var ContactUser = function() {
+var ContactUser = function () {
 
-  this.load = function() {
-    return browser.get("/#");
-  };
+	this.load = function () {
+		return browser.get("/#");
+	};
 
-  this.buttons = function() {
-    // @Todo - remove sleep() when https://github.com/angular/protractor/issues/2154
-    browser.sleep(500);
+	this.buttons = function () {
+		// @Todo - remove sleep() when https://github.com/angular/protractor/issues/2154
+		browser.sleep(500);
 
-    return element.all(by.css('[ng-click="cp.submitContact(item)"]'));
-  };
+		return element.all(by.css('[ng-click="cp.submitContact(item)"]'));
+	};
 
-  this.focusedButton = function() {
-    return browser.driver.switchTo().activeElement();
-  };
+	this.focusedButton = function () {
+		return browser.driver.switchTo().activeElement();
+	};
 
 
 };
 
 module.exports = ContactUser;
-
--- e2e/pages/UserDetails.js
+++ e2e/pages/UserDetails.js
@@ -1,11 +1,12 @@
-var UserDetails = function() {
-  this.load = function() { browser.get('/#'); };
+var UserDetails = function () {
+	this.load = function () {
+		browser.get('/#');
+	};
 
-  this.contactUser = function() {
-    $('button.contact').click();
-  };
+	this.contactUser = function () {
+		$('button.contact').click();
+	};
 
 };
 
 module.exports = UserDetails;
-
